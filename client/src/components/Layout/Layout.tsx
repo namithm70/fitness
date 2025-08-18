6e794b01034c9db8a31ae224f08c6a69d0532e36
namithm70 <namithm70@gmail.com>
2025-08-18 18:27:42 +0530

    Remove unused useEffect import to fix ESLint build error

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation, useNavigate, Outlet } from 'react-router-dom';
 import { 
   Home, 
